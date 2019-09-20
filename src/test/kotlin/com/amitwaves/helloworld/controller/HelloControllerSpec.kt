ef7b892d4b2eec51cd93c69bc89a66c5c616b65a
amitwaves <amitb74@hotmail.com>
2019-09-19 22:44:16 -0500

    add spek

---
+++ src/test/kotlin/com/amitwaves/helloworld/controller/HelloControllerSpec.kt
@@ -0,0 +1,24 @@
+package com.amitwaves.helloworld.controller
+
+import io.micronaut.context.ApplicationContext
+import io.micronaut.http.client.HttpClient
+import io.micronaut.runtime.server.EmbeddedServer
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object HelloControllerSpec: Spek({
+    describe("calling /hello endpoint") {
+        var server : EmbeddedServer = ApplicationContext.run(EmbeddedServer::class.java)
+        var client : HttpClient = HttpClient.create(server.url)
+
+        it("should respond Hello World from Kotlin on success") {
+            var expected = "Hello World from Kotlin on success"
+            var actual : String = client.toBlocking().retrieve("/hello")
+            actual.equals(expected)
+        }
+        afterGroup {
+            client.close()
+            server.close()
+        }
+    }
+})
